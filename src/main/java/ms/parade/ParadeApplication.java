014740e7254e2b2d657d0390741270f8bc586d47
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-09 14:49:46 +0900

    feat 초기 개발 환경 구성

---
+++ src/main/java/ms/parade/ParadeApplication.java
@@ -0,0 +1,13 @@
+package ms.parade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ms/parade/ParadeApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.parade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
