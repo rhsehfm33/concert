e52acdf5b82529b7afc05cccc1fa01d5166c5801
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-15 19:04:27 +0900

    feat seat reservation @Entity 추가

---
+++ src/main/java/ms/parade/domain/reservation/ReservationStatus.java
@@ -0,0 +1,6 @@
+package ms.parade.domain.reservation;
+
+public enum ReservationStatus {
+    COMPLETE,
+    CANCEL
+}
---
+++ src/main/java/ms/parade/infrastructure/reservation/SeatReservationEntity.java
@@ -0,0 +1,30 @@
+package ms.parade.infrastructure.reservation;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ms.parade.domain.reservation.ReservationStatus;
+
+@Entity
+@Getter
+@Table(name = "seat_reservations")
+@NoArgsConstructor
+public class SeatReservationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long userId;
+
+    private long seatId;
+
+    private Date date;
+
+    private ReservationStatus status;
+}
