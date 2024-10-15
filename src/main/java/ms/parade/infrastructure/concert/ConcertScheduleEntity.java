0164007e34125d22a5c8f3eb7db364aba6976652
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-15 18:34:25 +0900

    feat concert schedule @Entity 추가

---
+++ src/main/java/ms/parade/infrastructure/concert/ConcertScheduleEntity.java
@@ -0,0 +1,25 @@
+package ms.parade.infrastructure.concert;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "concert_schedules")
+@NoArgsConstructor
+public class ConcertScheduleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long concertId;
+
+    private Date performanceDate;
+}
