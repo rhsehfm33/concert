2d2326d75659b68ff45185b25d97124b814d6118
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-17 02:30:33 +0900

    feat 결제 정보 생성 기능 구현

--- src/main/java/ms/parade/infrastructure/payment/SeatPaymentEntity.java
+++ src/main/java/ms/parade/infrastructure/payment/SeatPaymentEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ms.parade.domain.payment.SeatPayment;
 
 @Entity
 @Getter
@@ -22,4 +23,21 @@ public class SeatPaymentEntity {
     private long seatReservationId;
 
     private String content;
+
+    public static SeatPaymentEntity from(SeatPaymentParams seatPaymentParams) {
+        SeatPaymentEntity seatPaymentEntity = new SeatPaymentEntity();
+        seatPaymentEntity.userId = seatPaymentParams.userId();
+        seatPaymentEntity.seatReservationId = seatPaymentParams.seatReservationId();
+        seatPaymentEntity.content = seatPaymentParams.content();
+        return seatPaymentEntity;
+    }
+
+    public static SeatPayment to(SeatPaymentEntity seatPaymentEntity) {
+        return new SeatPayment(
+            seatPaymentEntity.id,
+            seatPaymentEntity.userId,
+            seatPaymentEntity.seatReservationId,
+            seatPaymentEntity.content
+        );
+    }
 }
