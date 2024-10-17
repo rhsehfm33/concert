433eed2221b5c851b29f7647813649cee9d68baf
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-17 15:32:30 +0900

    fix JPA Repository 컴파일 오류 수정

--- src/main/java/ms/parade/infrastructure/concert/ConcertScheduleJpaRepository.java
+++ src/main/java/ms/parade/infrastructure/concert/ConcertScheduleJpaRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import jakarta.persistence.LockModeType;
 
@@ -14,5 +15,5 @@ public interface ConcertScheduleJpaRepository extends JpaRepository<ConcertSched
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT cs FROM ConcertScheduleEntity cs WHERE cs.id = :id")
-    Optional<ConcertScheduleEntity> findByIdForUpdate(Long concertId);
+    Optional<ConcertScheduleEntity> findByIdForUpdate(@Param("id") long id);
 }
