0a0b84ab9f66f00204f3478cc45907f3a227068a
rhsehfm33 <rhsehfm33@gmail.com>
2024-10-22 23:04:20 +0900

    test 변경된 잔여석 업데이트 로직에 따라 테스트 간소화

--- src/test/java/ms/parade/unit/domain/concert/ConcertServiceTest.java
+++ src/test/java/ms/parade/unit/domain/concert/ConcertServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import jakarta.persistence.EntityNotFoundException;
 import ms.parade.domain.concert.ConcertRepository;
 import ms.parade.domain.concert.ConcertSchedule;
 import ms.parade.domain.concert.ConcertService;
@@ -26,45 +24,9 @@ public class ConcertServiceTest {
     ConcertService concertService;
 
     @Test
-    public void addAvailableSeats_MinusToUnder0_IllegalArgumentException() {
-        ConcertSchedule concertSchedule = new ConcertSchedule(1, 1, 50, 2, LocalDate.now());
-        when(concertRepository.findScheduleByIdForUpdate(concertSchedule.id()))
-            .thenReturn(Optional.of(concertSchedule));
-        assertThrows(IllegalArgumentException.class,
-            () -> concertService.addAvailableSeats(concertSchedule.id(), -3));
-    }
-
-    @Test
     public void addAvailableSeats_MinusTo0_Success() {
         ConcertSchedule concertSchedule = new ConcertSchedule(1, 1, 50, 2, LocalDate.now());
-        when(concertRepository.findScheduleByIdForUpdate(concertSchedule.id()))
-            .thenReturn(Optional.of(concertSchedule));
         assertDoesNotThrow(() -> concertService.addAvailableSeats(concertSchedule.id(), -2));
-        verify(concertRepository).updateScheduleAvailableSeats(concertSchedule.id(), 0);
-    }
-
-    @Test
-    public void addAvailableSeats_AddOverMax_IllegalArgumentException() {
-        ConcertSchedule concertSchedule = new ConcertSchedule(1, 1, 50, 1, LocalDate.now());
-        when(concertRepository.findScheduleByIdForUpdate(concertSchedule.id()))
-            .thenReturn(Optional.of(concertSchedule));
-        assertThrows(IllegalArgumentException.class,
-            () -> concertService.addAvailableSeats(concertSchedule.id(), 50));
-    }
-
-    @Test
-    public void addAvailableSeats_AddToMax_Success() {
-        ConcertSchedule concertSchedule = new ConcertSchedule(1, 1, 50, 1, LocalDate.now());
-        when(concertRepository.findScheduleByIdForUpdate(concertSchedule.id()))
-            .thenReturn(Optional.of(concertSchedule));
-        assertDoesNotThrow(() -> concertService.addAvailableSeats(concertSchedule.id(), 49));
-        verify(concertRepository).updateScheduleAvailableSeats(concertSchedule.id(), 50);
-    }
-
-    @Test
-    public void addAvailableSeats_ScheduleNotExists_EntityNotFoundException() {
-        when(concertRepository.findScheduleByIdForUpdate(1)).thenReturn(Optional.empty());
-        assertThrows(EntityNotFoundException.class,
-            () -> concertService.addAvailableSeats(1, 50));
+        verify(concertRepository).updateScheduleAvailableSeats(concertSchedule.id(), -2);
     }
 }
