0d6aa636d4261157c4377e03d5f49f3c12cba773
rhsehfm33 <rhsehfm33@gmail.com>
2024-11-04 21:15:38 +0900

    fix 토큰 생성 HTTP method 변경

--- src/main/java/ms/parade/interfaces/queue/QueueTokenController.java
+++ src/main/java/ms/parade/interfaces/queue/QueueTokenController.java
@@ -3,7 +3,7 @@ package ms.parade.interfaces.queue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class QueueTokenController {
         return ResponseEntity.ok(queueTokenResponse);
     }
 
-    @PutMapping("/{userId}")
+    @PostMapping("/{userId}")
     ResponseEntity<QueueTokenResponse> putQueueToken(@PathVariable long userId) {
         QueueTokenInfo queueTokenInfo = queueTokenService.putUnique(userId);
         QueueTokenResponse queueTokenResponse = new QueueTokenResponse(queueTokenInfo);
