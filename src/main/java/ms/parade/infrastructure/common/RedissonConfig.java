cfd622cc13c2b88eee6112870d5cfbc983aef387
rhsehfm33 <rhsehfm33@gmail.com>
2024-11-26 16:13:08 +0900

    chore local spring 환경 docker 환경이랑 비슷하게 맞춤

--- src/main/java/ms/parade/infrastructure/common/RedissonConfig.java
+++ src/main/java/ms/parade/infrastructure/common/RedissonConfig.java
@@ -3,7 +3,6 @@ package ms.parade.infrastructure.common;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +16,7 @@ public class RedissonConfig {
     private int redisPort;
 
     @Bean
-    public RedissonClient redissonClient(@Autowired(required = false) EmbeddedRedisConfig embeddedRedisConfig) {
+    public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
         return Redisson.create(config);
--- src/main/java/ms/parade/infrastructure/queue/QueueTokenCrudRepository.java
+++ src/main/java/ms/parade/infrastructure/queue/QueueTokenCrudRepository.java
@@ -2,10 +2,11 @@ package ms.parade.infrastructure.queue;
 
 import java.util.List;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
+import lombok.NonNull;
+
 public interface QueueTokenCrudRepository extends CrudRepository<QueueTokenEntity, String> {
-    @NotNull
+    @NonNull
     List<QueueTokenEntity> findAll();
 }
