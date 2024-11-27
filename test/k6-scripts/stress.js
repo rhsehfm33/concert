43831536146529ce88986f1e3b28fdd58e189f5e
rhsehfm33 <rhsehfm33@gmail.com>
2024-11-27 23:19:11 +0900

    test 도커, 테스트 환경 리팩토링

--- test/k6-scripts/stress.js
+++ test/k6-scripts/stress.js
@@ -3,8 +3,8 @@ import {sleep} from 'k6';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 7000 },
-        { duration: '30s', target: 7000 },
+        { duration: '30s', target: 1000 },
+        { duration: '30s', target: 1000 },
     ],
     summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)'],
 };
@@ -12,11 +12,11 @@ export let options = {
 
 export default function() {
     const userId = __VU;
-    let req = http.post(`http://host.docker.internal:8080/v1/queue-tokens/${userId}`);
+    let req = http.post(`http://host.docker.internal:8081/v1/queue-tokens/${userId}`);
     try {
         const uuid = JSON.parse(req.body)['queueResult']['uuid'];
 
-        http.get(`http://host.docker.internal:8080/v1/queue-tokens/${uuid}`);
+        http.get(`http://host.docker.internal:8081/v1/queue-tokens/${uuid}`);
         sleep(10);
     } catch (e) {
         console.error(e);
